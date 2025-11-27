4ff292787207dca98fe515900e113576cd06e076
Juanda Patra <rahmad.patra@metrocomjaddi.com>
2025-11-27 14:45:56 +0700

    send data

--- components/TambahResepContainer/index.tsx
+++ components/TambahResepContainer/index.tsx
@@ -14,7 +14,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import {
   Select,
   SelectContent,
@@ -25,9 +25,13 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+// import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Toaster } from "../ui/sonner";
 
 const initialValue = {
   root: {
@@ -64,15 +68,15 @@ const formSchema = z.object({
   deskripsi: z.string().min(1, "Wajib diisi"),
   foto: z.any().optional(),
   sumber: z.string().min(1, "Pilih sumber resep"),
-  resep:  z.any(),
-})
-
-
+  resep: z.custom<SerializedEditorState>(() => true, {
+    message: "Isi resep tidak boleh kosong",
+  }),
+});
 
 function TambahResepContainer() {
   const [editorState, setEditorState] =
     useState<SerializedEditorState>(initialValue);
-    const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       nama: "",
@@ -80,122 +84,156 @@ function TambahResepContainer() {
       sumber: "",
       resep: "",
     },
-  })
+  });
 
-   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+
+    const formData = new FormData();
+    formData.append("name", values.nama);
+    formData.append("description", values.deskripsi);
+    formData.append("image", values.foto);
+    formData.append("source", values.sumber);
+    formData.append("ingredients", values.resep);
+
+    try {
+      const response = await fetch("http://localhost:8000/api/reseps", {
+        method: "POST",
+        body: formData,
+      });
+
+      const json = await response.json();
+
+      if (json.data) {
+        console.log("Success:", json.data);
+
+        toast.success(json.meta?.message || "Resep berhasil ditambahkan!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("Terjadi kesalahan pada server");
+    }
   }
 
   return (
-    <div className="mx-auto my-4">
-      <h1>Tambah Resep </h1>
+    <>
+      <div className="mx-auto my-4">
+        <h1>Tambah Resep </h1>
 
-       <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-4"
-      >
-        
-        {/* Nama */}
-        <FormField
-          control={form.control}
-          name="nama"
-          render={({ field }) => (
-            <FormItem className="grid w-full gap-3 pt-2">
-              <FormLabel>Nama</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="masukkan nama resep" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            {/* Nama */}
+            <FormField
+              control={form.control}
+              name="nama"
+              render={({ field }) => (
+                <FormItem className="grid w-full gap-3 pt-2">
+                  <FormLabel>Nama</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="masukkan nama resep"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Deskripsi Resep */}
-        <FormField
-          control={form.control}
-          name="deskripsi"
-          render={({ field }) => (
-            <FormItem className="grid w-full gap-3 pt-2">
-              <FormLabel>Deskripsi Resep</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="masukkan deskripsi resep" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+            {/* Deskripsi Resep */}
+            <FormField
+              control={form.control}
+              name="deskripsi"
+              render={({ field }) => (
+                <FormItem className="grid w-full gap-3 pt-2">
+                  <FormLabel>Deskripsi Resep</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="masukkan deskripsi resep"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Foto */}
-        <FormField
-          control={form.control}
-          name="foto"
-          render={({ field }) => (
-            <FormItem className="grid w-full gap-3 pt-2">
-              <FormLabel>Foto</FormLabel>
-              <FormControl>
-                <Input type="file" onChange={(e) => field.onChange(e.target.files?.[0])} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+            {/* Foto */}
+            <FormField
+              control={form.control}
+              name="foto"
+              render={({ field }) => (
+                <FormItem className="grid w-full gap-3 pt-2">
+                  <FormLabel>Foto</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="file"
+                      onChange={(e) => field.onChange(e.target.files?.[0])}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Select Source */}
-        <FormField
-          control={form.control}
-          name="sumber"
-          render={({ field }) => (
-            <FormItem className="grid w-full gap-3 pt-2">
-              <FormLabel>Source/Sumber</FormLabel>
-              <FormControl>
-                <Select onValueChange={field.onChange}>
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Sumber resep" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectItem value="youtube">Youtube</SelectItem>
-                      <SelectItem value="tiktok">Tiktok</SelectItem>
-                      <SelectItem value="instagram">Instagram</SelectItem>
-                      <SelectItem value="buku">Buku Resep</SelectItem>
-                      <SelectItem value="sendiri">Buat Sendiri</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+            {/* Select Source */}
+            <FormField
+              control={form.control}
+              name="sumber"
+              render={({ field }) => (
+                <FormItem className="grid w-full gap-3 pt-2">
+                  <FormLabel>Source/Sumber</FormLabel>
+                  <FormControl>
+                    <Select onValueChange={field.onChange}>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Sumber resep" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="youtube">Youtube</SelectItem>
+                          <SelectItem value="tiktok">Tiktok</SelectItem>
+                          <SelectItem value="instagram">Instagram</SelectItem>
+                          <SelectItem value="buku">Buku Resep</SelectItem>
+                          <SelectItem value="sendiri">Buat Sendiri</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Isi Resep (Text Editor) */}
-        <FormField
-          control={form.control}
-          name="resep"
-          render={({ field }) => (
-            <FormItem className="grid w-full gap-3 pt-2">
-              <FormLabel>Resep</FormLabel>
-              <FormControl>
-                <Editor
-                  editorSerializedState={field.value}
-                  onSerializedChange={field.onChange}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+            {/* Isi Resep (Text Editor) */}
+            <FormField
+              control={form.control}
+              name="resep"
+              render={({ field }) => (
+                <FormItem className="grid w-full gap-3 pt-2">
+                  <FormLabel>Resep</FormLabel>
+                  <FormControl>
+                    <Editor
+                      editorSerializedState={field.value}
+                      onSerializedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Submit Button */}
-        <div className="flex justify-end pt-3">
-          <Button type="submit">Tambah Resep</Button>
-        </div>
-
-      </form>
-    </Form>
-     
-    </div>
+            {/* Submit Button */}
+            <div className="flex justify-end pt-3">
+              <Button type="submit">Tambah Resep</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+      <Toaster position="top-right" richColors closeButton />
+    </>
   );
 }
 
